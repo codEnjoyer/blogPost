6ab34f77236b8b66c46bc16eb3983354be5a0c19
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-03 18:55:59 +0500

    Изменил подключение к базе данных на асинхронное

--- src/database.py
+++ src/database.py
@@ -1,16 +1,18 @@
-from sqlalchemy import MetaData, create_engine
+from typing import AsyncGenerator
+
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
 
 from config import DB_URL
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
-engine = create_engine(DB_URL)
-session_maker = sessionmaker(engine, expire_on_commit=False)
+engine = create_async_engine(DB_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_session() -> Session:
-    with session_maker() as session:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
         yield session
