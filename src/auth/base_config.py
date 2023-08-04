2714d368949a012da74a049cec82bdacd4f5b636
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-04 19:31:57 +0500

    Вынес получение текущего пользователя в зависимости

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -23,5 +23,3 @@ fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
     [auth_backend],
 )
-
-current_user = fastapi_users.current_user()
--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from auth.base_config import fastapi_users
 from database import get_async_session
 from auth.manager import UserManager
 from users.models import User
@@ -15,3 +16,7 @@ async def get_user_db(session: Annotated[AsyncSession, Depends(get_async_session
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
+
+
+async def get_current_user():
+    yield fastapi_users.current_user()
