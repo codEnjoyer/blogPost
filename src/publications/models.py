67ee8ffe15aebfa4f24e0daf49d391e685d22b92
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-11 18:12:23 +0500

    Починил функциональность

--- src/publications/models.py
+++ src/publications/models.py
@@ -16,4 +16,5 @@ class Publication(Base):
     last_edit_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
 
     author: Mapped["User"] = relationship(back_populates="publications")
-    reactions: Mapped[list["Reaction"]] = relationship(back_populates="publication", cascade="all, delete-orphan")
+    reactions: Mapped[list["Reaction"]] = relationship(back_populates="publication", cascade="all, delete-orphan",
+                                                       lazy="joined")
