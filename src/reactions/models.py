40bea6fbf25fd0d49fb5600939ffd1d62a2cb060
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-11 17:18:35 +0500

    Добавил время публикации реакции в базу данных

--- src/reactions/models.py
+++ src/reactions/models.py
@@ -1,6 +1,6 @@
 from enum import StrEnum
 
-from sqlalchemy import Integer, ForeignKey, Enum
+from sqlalchemy import Integer, ForeignKey, Enum, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
@@ -17,6 +17,7 @@ class Reaction(Base):
     publication_id: Mapped[int] = mapped_column(Integer, ForeignKey("publication.id"), nullable=False, primary_key=True)
     type: Mapped[ReactionType] = mapped_column(Enum(ReactionType), nullable=False)
     author_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"), nullable=False, primary_key=True)
+    published_at: Mapped[int] = mapped_column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
 
     author: Mapped["User"] = relationship(back_populates="reactions")
     publication: Mapped["Publication"] = relationship(back_populates="reactions")
