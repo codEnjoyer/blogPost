7bf4f72d9b48edee8263f0d384ec2c9995e66066
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-14 21:19:23 +0500

    Добавляет исключения при работе с публикациями

--- src/publications/exceptions.py
+++ src/publications/exceptions.py
@@ -2,6 +2,18 @@ from fastapi import HTTPException, status
 
 
 class PublicationNotFound(HTTPException):
-    def __init__(self, *, reaction_id: int):
+    def __init__(self, *, publication_id: int):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND,
-                         detail=f"Publication with id {reaction_id} was not found")
+                         detail=f"Publication with id {publication_id} was not found")
+
+
+class NotAuthorOfPublicationException(HTTPException):
+    def __init__(self, *, publication_id: int):
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN,
+                         detail=f"You are not the author of publication with id {publication_id}")
+
+
+class SelfReactionException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST,
+                         detail="You can't react to your own publication")
--- src/publications/router.py
+++ src/publications/router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter
 
 from publications.crud import (
     get_all_publications, get_publication_by_id, create_publication,
     delete_publication_from_db, update_publication)
+from publications.exceptions import PublicationNotFound, NotAuthorOfPublicationException
 from publications.schemas import PublicationRead, PublicationCreate, PublicationUpdate
 from utils.types import AsyncDBSession, QueryDBLimit, QueryDBOffset, PathID, CurrentUser
 
@@ -22,8 +23,7 @@ async def get_publication(publication_id: PathID,
                           db: AsyncDBSession) -> PublicationRead | None:
     publication = await get_publication_by_id(db, publication_id)
     if not publication:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Publication was not found")
+        raise PublicationNotFound(publication_id=publication_id)
     return publication
 
 
@@ -41,11 +41,9 @@ async def delete_publication(publication_id: PathID,
                              db: AsyncDBSession) -> PublicationRead:
     publication_to_delete = await get_publication_by_id(db, publication_id)
     if not publication_to_delete:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Publication was not found")
+        raise PublicationNotFound(publication_id=publication_id)
     if publication_to_delete.author_id != user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="You are not the author of this publication")
+        raise NotAuthorOfPublicationException(publication_id=publication_id)
     deleted_publication = await delete_publication_from_db(db, publication_to_delete)
     return deleted_publication
 
@@ -57,10 +55,8 @@ async def edit_publication(publication_id: PathID,
                            db: AsyncDBSession) -> PublicationRead:
     publication_to_edit = await get_publication_by_id(db, publication_id)
     if not publication_to_edit:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Publication was not found")
+        raise PublicationNotFound(publication_id=publication_id)
     if publication_to_edit.author_id != user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="You are not the author of this publication")
+        raise NotAuthorOfPublicationException(publication_id=publication_id)
     edited_publication = await update_publication(db, publication_to_edit, publication)
     return edited_publication
