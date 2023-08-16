47cb14853e97ee71040b8832ba68b208efb08b10
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-08-16 14:34:02 +0500

    Возвращает head в проведение миграции

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd src || exit
-alembic upgrade 4836c0df2d4a
+alembic upgrade head
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --timeout 600
